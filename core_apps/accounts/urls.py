cd34eb3126fc812d12a787df70441682a892a386
ShuhaibMuhammad <mshuhaib175@gmail.com>
2025-12-28 11:41:14 +0530

    feat: customer can withdraw their money

--- core_apps/accounts/urls.py
+++ core_apps/accounts/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import (
     AccountVerificationView,
     DepositView,
-    # InitiateWithdrawalView,
-    # VerifyUsernameAndWithdrawAPIView,
+    InitiateWithdrawalView,
+    VerifyUsernameAndWithdrawAPIView,
     # InitiateTransferView,
     # VerifyOTPView,
     # VerifySecurityQuestionView,
@@ -18,16 +18,16 @@ urlpatterns = [
         name="account_verification",
     ),
     path("deposit/", DepositView.as_view(), name="account_deposit"),
-    # path(
-    #     "initiate-withdrawal/",
-    #     InitiateWithdrawalView.as_view(),
-    #     name="initiate_withdrawal",
-    # ),
-    # path(
-    #     "verify-username-and-withdraw/",
-    #     VerifyUsernameAndWithdrawAPIView.as_view(),
-    #     name="verify_username_and_withdraw",
-    # ),
+    path(
+        "initiate-withdrawal/",
+        InitiateWithdrawalView.as_view(),
+        name="initiate_withdrawal",
+    ),
+    path(
+        "verify-username-and-withdraw/",
+        VerifyUsernameAndWithdrawAPIView.as_view(),
+        name="verify_username_and_withdraw",
+    ),
     # path(
     #     "transfer/initiate/", InitiateTransferView.as_view(), name="initiate_transfer"
     # ),
