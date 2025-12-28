ae9553bd9e8f10286f0bcb735668161630004fa6
ShuhaibMuhammad <mshuhaib175@gmail.com>
2025-12-28 15:31:43 +0530

    feat: customer can transfer money to other users

--- core_apps/accounts/urls.py
+++ core_apps/accounts/urls.py
@@ -4,9 +4,9 @@ from .views import (
     DepositView,
     InitiateWithdrawalView,
     VerifyUsernameAndWithdrawAPIView,
-    # InitiateTransferView,
-    # VerifyOTPView,
-    # VerifySecurityQuestionView,
+    InitiateTransferView,
+    VerifyOTPView,
+    VerifySecurityQuestionView,
     # TransactionListAPIView,
     # TransactionPDFView,
 )
@@ -28,15 +28,15 @@ urlpatterns = [
         VerifyUsernameAndWithdrawAPIView.as_view(),
         name="verify_username_and_withdraw",
     ),
-    # path(
-    #     "transfer/initiate/", InitiateTransferView.as_view(), name="initiate_transfer"
-    # ),
-    # path(
-    #     "transfer/verify-security-question/",
-    #     VerifySecurityQuestionView.as_view(),
-    #     name="verify_security_question",
-    # ),
-    # path("transfer/verify-otp/", VerifyOTPView.as_view(), name="verify_otp"),
+    path(
+        "transfer/initiate/", InitiateTransferView.as_view(), name="initiate_transfer"
+    ),
+    path(
+        "transfer/verify-security-question/",
+        VerifySecurityQuestionView.as_view(),
+        name="verify_security_question",
+    ),
+    path("transfer/verify-otp/", VerifyOTPView.as_view(), name="verify_otp"),
     # path("transactions/", TransactionListAPIView.as_view(), name="transaction_list"),
     # path("transactions/pdf/", TransactionPDFView.as_view(), name="transaction_pdf"),
 ]
