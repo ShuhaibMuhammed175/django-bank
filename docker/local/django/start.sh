946e7f700ecba17a7d9ab5a1bacb3ae8f4ecb50c
ShuhaibMuhammad <mshuhaib175@gmail.com>
2026-01-02 21:46:45 +0530

    Final push

--- docker/local/django/start.sh
+++ docker/local/django/start.sh
@@ -9,3 +9,9 @@ set -o nounset
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 exec python manage.py runserver 0.0.0.0:8000
+
+#exec gunicorn config.wsgi:application \
+ #    --bind 0.0.0.0:8000 \
+ #    --workers 3 \
+ #    --timeout 120 \
+ #    --log-level info
